5488f05529c1bf240511e3a6c7d9a4b74d572fab
Mateus Macedo <mhbassomacedo@gmail.com>
2019-12-01 06:26:22 -0300

    Create search by Value of apartment
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- back/app/consultMap.js
+++ back/app/consultMap.js
@@ -26,9 +26,62 @@ app.get('/consultaPorID', function (req, res) {
 
 });
 
-app.get('/consultaGeral', function (req, res) {
+app.get("/consultarGeral", (req, res) => {
+
     const body = req.body
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("loftech");
+
+        dbo.collection("apartament").find({ price: { $lt: body.price } })
+            .toArray(async (err, result) => {
+                if (err) throw err
+                console.log(result)
+                res.status(200).send(result)
+
+            })
+    })
+
 });
+
+function adicionarValorRandom(req, res) {
+    const body = req.body
+
+    //body.valor
+    //body.public
+    //body.hospital
+    const high = 5000000
+    const low = 600000
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("loftech");
+
+        dbo.collection("apartament").find()
+            .toArray(async (err, result) => {
+                if (err) throw err
+
+
+                console.log("bbb")
+                let mapResult = result.map(async item => {
+                    //console.log(url_api)
+                    let price = Math.random() * (high - low) + low
+                    await dbo.collection("apartament").updateOne({ "_id": item._id }, { $set: { price } })
+                })
+
+                //dbo.collection("apartament").update("")
+                await Promise.all(mapResult)
+
+                //db.close()
+                console.log("Fim do programa")
+                res.send("OK")
+
+            })
+
+    })
+
+};
 
 
 app.listen(3000, function () {
