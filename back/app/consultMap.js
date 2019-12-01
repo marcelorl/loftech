bf348ee22c2a3a8003564c833bf1a778958b7792
Mateus Macedo <mhbassomacedo@gmail.com>
2019-12-01 07:26:39 -0300

    Change the parameter of API
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- back/app/consultMap.js
+++ back/app/consultMap.js
@@ -8,14 +8,15 @@ const { ObjectId } = require('mongodb'); // or ObjectID
 var url = "mongodb://localhost:27017/";
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/consultaPorID', function (req, res) {
-    const body = req.body
-    console.log(body)
+app.get('/consultaid/:id', function (req, res) {
+
+    console.log(req.params)
+
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("loftech");
 
-        dbo.collection("apartament").find({ _id: ObjectId(body.id) })
+        dbo.collection("apartament").find({ _id: ObjectId(req.params.id) })
             .toArray(async (err, result) => {
                 if (err) throw err
                 console.log(result)
@@ -25,6 +26,10 @@ app.get('/consultaPorID', function (req, res) {
     })
 
 });
+
+app.get("/hello", (req, res) => {
+    res.send("HELLO")
+})
 
 app.get("/consultarGeral", (req, res) => {
 
