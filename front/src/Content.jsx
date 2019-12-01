d6cb1f923e85c3d58adfbd974cbec4832ad2ddd9
marcelorl <marcelo_rl2@hotmail.com>
2019-12-01 03:17:59 -0300

    first commit

---
+++ front/src/Content.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ApartmentItem from './ApartmentItem'
+
+export default () => {
+    return (
+        <ApartmentItem />
+    )
+}
---
+++ front/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Content from './Content';
+
+ReactDOM.render(<App><Content /></App>, document.getElementById('root'));
